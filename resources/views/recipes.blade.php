f7b46b4193aa23f70c6626b98c7884686bfb1326
wachcio <wachcio@poczta.fm>
2020-04-29 00:38:54 +0200

    Initial commit

---
+++ resources/views/recipes.blade.php
@@ -0,0 +1,11 @@
+{{-- @foreach($recipes as $key => $data)
+    <tr>
+      <th>{{$data->ID}}</th>
+      <th>{{$data->name}}</th>
+      <th>{{$data->ingredients}}</th>
+      <th>{{$data->picture}}</th>
+      <th>{{$data->rating}}</th>
+    </tr>
+@endforeach --}}
+
+{{ $recipes }}
