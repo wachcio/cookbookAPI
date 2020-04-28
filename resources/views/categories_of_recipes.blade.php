03283e5c9f982c975221ac85bb498329da701739
wachcio <wachcio@poczta.fm>
2020-04-29 01:57:45 +0200

    add categories of recipes

---
+++ resources/views/categories_of_recipes.blade.php
@@ -0,0 +1,1 @@
+{{ $categories_of_recipes }}
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,11 @@ Route::get('/categories', function () {
 
     return view('categories', ['categories'=> $categories]);
 });
+
+Route::get('/categories_of_recipes', function () {
+
+    // $categories_of_recipes = DB::table('recipes_id_category_id_view')->get();
+    $categories_of_recipes = DB::table('recipes_id_category_id')->select('recipes_id_category_id.ID', 'recipes.name', 'categories.category_name')->join('recipes', 'recipes.ID', '=', 'recipes_id_category_id.recipes_id')->join('categories', 'categories.ID', '=', 'recipes_id_category_id.category_id')->get();
+
+    return view('categories_of_recipes', ['categories_of_recipes'=> $categories_of_recipes]);
+});
