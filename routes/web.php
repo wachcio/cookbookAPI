310e12fff68369c3e23675bbf434681465552a34
wachcio <wachcio@poczta.fm>
2020-04-29 03:23:22 +0200

    categories_of_recipes works

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 
 Route::get('/recipes', function () {
     $recipes = DB::table('recipes')->get();
+    // var_dump($recipes);
 
     return view('recipes', ['recipes'=> $recipes]);
 });
@@ -31,18 +32,15 @@ Route::get('/categories', function () {
 });
 
 Route::get('/categories_of_recipes', function () {
-    // $sql = "SELECT recipes_id_category_id.ID, recipes.ID AS recipes_ID, recipes.name, GROUP_CONCAT(categories.category_name ORDER BY categories.category_name SEPARATOR ', ') AS categories FROM recipes_id_category_id INNER JOIN recipes on recipes.ID = recipes_id_category_id.recipes_id INNER JOIN categories on categories.ID = recipes_id_category_id.category_id GROUP BY recipes.name ORDER BY recipes.name ASC";
 
-    // $categories_of_recipes = DB::select(DB::raw($sql));
 
     $categories_of_recipes = DB::table('recipes_id_category_id')
-    ->select('recipes_id_category_id.ID', 'recipes.ID AS recipes_ID', 'ecipes.name', 'DB::raw("GROUP_CONCAT(categories.category_name ORDER BY categories.category_name SEPARATOR ', ')")')
+    ->select('recipes_id_category_id.ID', 'recipes.ID AS recipes_ID', 'recipes.name', DB::raw('GROUP_CONCAT(categories.category_name ORDER BY categories.category_name SEPARATOR \', \') AS categories'))
     ->join('recipes','recipes.ID', '=', 'recipes_id_category_id.recipes_id')
     ->join('categories','categories.ID', '=', 'recipes_id_category_id.category_id')
     ->groupBy('recipes.name')
-    ->orderBy('recipes.name');
-    //select recipes_id_category_id.ID, recipes.ID AS recipes_ID, recipes.name, GROUP_CONCAT(categories.category_name ORDER BY categories.category_name SEPARATOR ', ') AS categories from recipes_id_category_id inner join recipes on recipes.ID = recipes_id_category_id.recipes_id inner join categories on categories.ID = recipes_id_category_id.category_id GROUP by recipes.name ORDER BY recipes.name ASC
-    // $categories_of_recipes = DB::table('recipes_id_category_id')->select('recipes_id_category_id.ID', 'recipes.name', 'categories.category_name')->join('recipes', 'recipes.ID', '=', 'recipes_id_category_id.recipes_id')->join('categories', 'categories.ID', '=', 'recipes_id_category_id.category_id')->get();
+    ->orderBy('recipes.name')
+    ->get();
 
     return view('categories_of_recipes', ['categories_of_recipes'=> $categories_of_recipes]);
 });
